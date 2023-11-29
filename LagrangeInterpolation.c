d00aa2da5fb2e61a4480d5b39f09e98417bebc81
Ebik <75691501+NickName-AM@users.noreply.github.com>
2023-11-29 21:27:33 +0545

    Add files via upload

---
+++ LagrangeInterpolation.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define SIZE 100
+
+int main()
+{
+	float x[SIZE], y[SIZE], q, p=0, l;
+	int n;
+	printf("Enter number of data: ");
+	scanf("%d", &n);
+	
+	for(int i=0;i<n;i++)
+	{
+		printf("x[%d] = ", i);
+		scanf("%f", &x[i]);
+		printf("f[%d] = ", i);
+		scanf("%f", &y[i]);
+	}
+	 
+	printf("Enter the interpolation point: ");
+	scanf("%f", &q);
+
+	for(int i=0;i<n;i++)
+	{
+		l=1;
+		for(int j=0;j<n;j++)
+		{
+			if(i!=j)
+			{
+				l = l * (q - x[j])/(x[i] - x[j]);
+			}
+		}
+		p = p + l * y[i];
+	}
+
+	printf("Interpolated value at %.3f is %.5f.\n\n\n", q, p);
+	return 0;
+}
